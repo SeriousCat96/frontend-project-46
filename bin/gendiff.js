a33f3b92f56fa958f55fdaae66a40f0417cae33c
Pavel Shevelev <pavel.shevelev@akbarsdigital.ru>
2023-05-23 01:55:08 +0300

    init repo

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .name('gendiff')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse();
