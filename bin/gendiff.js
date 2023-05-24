efcf19b784cb61f28b22fdf20f4c17303ead756d
Pavel Shevelev <pavel.shevelev@akbarsdigital.ru>
2023-05-24 18:56:00 +0300

    add format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,8 @@ const program = new Command();
 program
   .name('gendiff')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .argument('<type>');
 
 program.parse();
